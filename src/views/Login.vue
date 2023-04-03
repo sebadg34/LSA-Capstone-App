ed2729c05e70a1bccda3666dc30c05ec77b30a8d
Sebastian <sebadg98@gmail.com>
2023-04-03 13:13:47 -0400

    feat: agregado archivos y librerias (bootstrap-vue) para la inicializacion del proyecto. Agregado archivo estilado global main.scss

---
+++ src/views/Login.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="login">
+    <b-container>
+        <b-skeleton-wrapper :loading="loading">
+            <template #loading>
+                <br>
+                <br>
+                <br>
+                <b-row align-h="center">
+                    <b-col sm="6">
+                        <b-icon icon="stopwatch" font-scale="3" animation="cylon"></b-icon>
+                        <p id="cancel-label">Por favor espere...</p>
+                    </b-col>
+                </b-row>
+
+            </template>
+
+            <b-row style="text-align:center;" class="text-center" align-h="center">
+                <br>
+                <b-col sm="12" xs="12" lg="6">
+                    <b-card header-class="lsa-blue" header-tag="header" header="Primary" text-variant="white">
+                      
+
+                       
+                                        <b-input-group class="mb-2">
+                                            <b-input-group-prepend is-text>
+                                                <b-icon icon="person-fill"></b-icon>
+                                            </b-input-group-prepend>
+                                            <b-form-input class="input-rut" name="rut" id="rut" type="text" aria-describedby="rut-live-feedback"  placeholder="Rut"></b-form-input>
+                                          
+                                        </b-input-group>
+                                 
+
+                                   
+                                        <b-input-group class="mb-2">
+                                            <b-input-group-prepend is-text>
+                                                <b-icon icon="lock-fill"></b-icon>
+                                            </b-input-group-prepend>
+                                            <b-form-input class="input-pass" name="password" type="password" aria-describedby="password-live-feedback"  placeholder="Contraseña"></b-form-input>
+                                         
+                                        </b-input-group>
+                                  
+
+                                    <br>
+                                    
+                                    <b-button id="button-interactive"  type="submit" block style="font-weight: bold; font-size: 1.2rem;  background-color: var(--lsa-orange) !important; " pill>
+                                        <b-spinner v-if="loading" small variant="primary" label="Spinning">
+                                        </b-spinner> Iniciar Sesión <b-icon icon="box-arrow-in-right" aria-hidden="true"></b-icon>
+                                    </b-button>
+
+                    
+                    </b-card>
+                </b-col>
+            </b-row>
+        </b-skeleton-wrapper>
+    </b-container>
+</div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        loading: false
+      }
+    },
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
